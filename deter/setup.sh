f08f3bd5cd44bccaf04387180f152bac520a4519
Amir Fahat <amirf@mit.edu>
2021-01-04 15:37:57 -0500

    Add setup script to topology ns file

--- deter/setup.sh
+++ deter/setup.sh
@@ -1,5 +1,5 @@
 # Expect:
-# 1. Californium cloned in /proj/MIT-DoS/exp/coap-setup/deps/
+# 1. Forked californium cloned in /proj/MIT-DoS/exp/coap-setup/deps/ from source: https://github.com/amirfarhat/californium
 
-# Install Java JDK, JRE, and Maven
-sudo apt install -y default-jdk default-jre maven
+# Install Java: JDK, JRE
+sudo apt install -y default-jdk default-jre
