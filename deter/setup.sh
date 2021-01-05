8c6eb021d71e3f02667827d5540671d88c55435f
Amir Fahat <amirf@mit.edu>
2021-01-05 11:32:48 -0500

    Consolidate single location for setup script

--- deter/setup.sh
+++ deter/setup.sh
@@ -1,5 +1,8 @@
 # Expect:
 # 1. Forked californium cloned in /proj/MIT-DoS/exp/coap-setup/deps/ from source: https://github.com/amirfarhat/californium
 
+# Install network utilities
+sudo apt install -y iperf traceroute
+
 # Install Java: JDK, JRE
 sudo apt install -y default-jdk default-jre
