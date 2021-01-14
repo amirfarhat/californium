e3fd195a20f72406ba690129db147ab0520383ec
Amir Fahat <amirf@mit.edu>
2021-01-14 12:57:52 -0500

    Add proxy stresstest materials

---
+++ deter/multislowserver.py
@@ -0,0 +1,51 @@
+"""
+Multithreaded HTTP server with artificial delay for requests
+Based on https://stackoverflow.com/a/14089457/10028710
+"""
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+import threading
+
+import time
+import socket
+import argparse
+
+from struct import pack
+
+def parse_args():
+  """
+  Handle command-line arguments
+  """
+  parser = argparse.ArgumentParser(description = 'Launch a multithreaded server with induced artifical delay per request')
+  
+  parser.add_argument('-p', '--port', dest='port',
+                      help='port to launch the server on',
+                      action='store', default=80, type=int)
+  parser.add_argument('-d', '--delay', dest='delay',
+                      help='Artifical delay to include per request',
+                      action='store', default=0, type=float)
+  
+  args = parser.parse_args()
+  return args
+
+args = parse_args()
+
+def be_slow(seconds):
+  time.sleep(float(seconds))
+
+class Handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    be_slow(args.delay)
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.end_headers()
+    self.wfile.write(str.encode("I'm a slow response LOL\n"))
+    return
+
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+if __name__ == '__main__':
+    server = ThreadingHTTPServer(('localhost', args.port), Handler)
+    server.serve_forever()
---
+++ stressproxy.sh
@@ -0,0 +1,22 @@
+# Number of parallel requests to make
+N=$1
+
+# HTTP URL to access
+httpdst=$2
+
+proxycmd="java -jar demo-apps/run/cf-proxy2-3.0.0-SNAPSHOT.jar ExampleProxy2CoapClient $httpdst"
+
+rm outs/*
+
+# Execute N requests via the proxy asynchronously
+for ((i = 0 ; i < $N ; i++)); do
+  eval "$proxycmd/$i" > "outs/out$i.txt" &
+  pids[${i}]=$!
+  echo "Sent Req ${i}"
+done
+
+# Wait on all requests to complete
+echo "Waiting for responses"
+for pid in ${pids[*]}; do
+  wait $pid
+done
