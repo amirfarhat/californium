f58ee50e79f13f96ec7ed102550f85593517745a
Amir Fahat <amirf@mit.edu>
2021-01-14 16:01:47 -0500

    Modify proxy tester to take in arbitrary endpoint

--- stressproxy.sh
+++ stressproxy.sh
@@ -4,14 +4,19 @@ N=$1
 # HTTP URL to access
 httpdst=$2
 
-proxycmd="java -jar demo-apps/run/cf-proxy2-3.0.0-SNAPSHOT.jar ExampleProxy2CoapClient $httpdst"
+# CoAP endpoint of proxy without port
+proxyendpoint=$3
 
 mkdir -p outs
 rm outs/*
 
+proxycmdprefix="java -jar demo-apps/run/cf-proxy2-3.0.0-SNAPSHOT.jar ExampleProxy2CoapClient"
+
 # Execute N requests via the proxy asynchronously
 for ((i = 0 ; i < $N ; i++)); do
-  eval "$proxycmd/$i" > "outs/out$i.txt" &
+  proxycmd="$proxycmdprefix $httpdst/$i $proxyendpoint"
+  echo $proxycmd
+  eval "$proxycmd" > "outs/out$i.txt" &
   pids[${i}]=$!
   echo "Sent Req ${i}"
 done
@@ -21,3 +26,5 @@ echo "Waiting for responses"
 for pid in ${pids[*]}; do
   wait $pid
 done
+
+cat outs/out0.txt
