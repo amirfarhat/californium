62a4d9407901830a683591f2f50da353c7fa7b40
Amir Fahat <amirf@mit.edu>
2021-01-15 14:28:45 -0500

    Update topology and expand versatility of the proxy launch script

--- stressproxy.sh
+++ stressproxy.sh
@@ -7,21 +7,29 @@ httpdst=$2
 # CoAP endpoint of proxy without port
 proxyendpoint=$3
 
-mkdir -p outs
-rm outs/*
+# Name of output directory
+outdir=$4
+
+# Flag to wait for responses
+awaitresponses=$5
+
+mkdir -p $outdir
+rm $outdir/*
 
 proxycmdprefix="java -jar demo-apps/run/cf-proxy2-3.0.0-SNAPSHOT.jar ExampleProxy2CoapClient"
 
 # Execute N requests via the proxy asynchronously
 for ((i = 0 ; i < $N ; i++)); do
   proxycmd="$proxycmdprefix $httpdst/$i $proxyendpoint"
-  eval "$proxycmd" > "outs/out$i.txt" &
+  eval "$proxycmd" > "$outdir/out$i.txt" &
   pids[${i}]=$!
   echo "Sent Req ${i}"
 done
 
 # Wait on all requests to complete
-echo "Waiting for responses"
-for pid in ${pids[*]}; do
-  wait $pid
-done
+if [ ! -z "$awaitresponses" ]; then
+  echo "Waiting for responses"
+  for pid in ${pids[*]}; do
+    wait $pid
+  done
+fi
