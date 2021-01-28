a41322e8a6337648551e254768b32e7a6fa99356
Amir Farhat <amirf@users.isi.deterlab.net>
2021-01-28 13:01:33 -0800

    Deter checkpoint

--- deter/scripts/run.sh
+++ deter/scripts/run.sh
@@ -2,7 +2,7 @@
 . /proj/MIT-DoS/exp/coap-setup/deps/californium/deter/scripts/config.sh
 
 # Options
-while getopts ":v:n:" opt; do
+while getopts ":vn:" opt; do
   case "$opt" in
     "v") _V=1;;
     "n") n=$OPTARG;;
@@ -17,45 +17,18 @@ function log () {
 }
 
 mkdir -p $TMP_DATA
-mdkir -p $DETER_HOME/expdata/real/$n
+mkdir -p $DETER_HOME/expdata/real/$n
 data_dir=$DETER_HOME/expdata/real/$n
+log "Created data directory $data_dir\n"
 
 # Kill all detached remote sessions
 for host_name in ${HOST_NAMES[@]}; do
-  log "Killing detached sessions on $host_name..."
+  log "Killing detached sessions on $host_name...\n"
   ssh $RUN_USER@$host_name "$BIN_HOME/kill_detached.sh"
-  log "OK"
+  log "OK\n"
 done
 
 # Origin server
 log "[SETUP] Starting origin server...\n"
 ssh $RUN_USER@$PROXY_NAME "$SCRIPTS_HOME/start_origin_server.sh -v"
-log "OK"
-
-# Proxy
-log "[SETUP] Starting proxy...\n"
-ssh $RUN_USER@$PROXY_NAME "$SCRIPTS_HOME/start_proxy.sh -v"
-log "OK"
-
-log "Sleeping for a bit...\n"
-sleep 5
-log "OK"
-
-# Attacker
-log "[SETUP] Starting attackers...\n"
-ssh $RUN_USER@$ATTACKER_NAME "$SCRIPTS_HOME/start_attacker.sh -v"
-log "OK"
-
-sleep_amt=$(( $ATTACKER_DURATION + $ORIGIN_SERVER_DURATION + $PROXY_DURATION ))
-log "[SETUP] Waiting for $sleep_amt seconds...\n"
-sleep $sleep_amt
-log "OK"
-
-# Move data files from tmp into the data directory
-for host_name in ${HOST_NAMES[@]}; do
-  log "Moving data from $host_name to $data_dir..."
-  ssh $RUN_USER@$host_name "$BIN_HOME/move_data.sh $data_dir"
-  log "OK"
-done
-
-log "Finished experiment!"
+log "OK\n"
