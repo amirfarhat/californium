05c803370a104503cd2e7ae095692c5b9785cd6b
Amir Farhat <amirf@users.isi.deterlab.net>
2021-01-28 13:05:01 -0800

    Deter checkpoint full run

--- deter/scripts/run.sh
+++ deter/scripts/run.sh
@@ -32,3 +32,31 @@ done
 log "[SETUP] Starting origin server...\n"
 ssh $RUN_USER@$PROXY_NAME "$SCRIPTS_HOME/start_origin_server.sh -v"
 log "OK\n"
+
+# Proxy
+log "[SETUP] Starting proxy...\n"
+ssh $RUN_USER@$PROXY_NAME "$SCRIPTS_HOME/start_proxy.sh -v"
+log "OK\n"
+
+log "Sleeping for a bit...\n"
+sleep 5
+log "OK\n"
+
+# Attacker
+log "[SETUP] Starting attackers...\n"
+ssh $RUN_USER@$ATTACKER_NAME "$SCRIPTS_HOME/start_attacker.sh -v"
+log "OK\n"
+
+sleep_amt=$(( $PROXY_DURATION ))
+log "[SETUP] Waiting for $sleep_amt seconds...\n"
+sleep $sleep_amt
+log "OK\n"
+
+# Move data files from tmp into the data directory
+for host_name in ${HOST_NAMES[@]}; do
+  log "Moving data from $host_name to $data_dir...\n"
+  ssh $RUN_USER@$host_name "$BIN_HOME/move_data.sh $data_dir"
+  log "OK\n"
+done
+
+log "Finished experiment!\n"
