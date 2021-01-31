057d5662325d91322cad0e78687caec33d2e5d9e
Amir Fahat <amirf@mit.edu>
2021-01-30 20:00:24 -0500

    Include apache logs

--- deter/scripts/sbin/origin_server_run.sh
+++ deter/scripts/sbin/origin_server_run.sh
@@ -6,9 +6,14 @@ rm -f $TMP_DATA/$ORIGIN_SERVER_LOGNAME
 touch $TMP_DATA/$ORIGIN_SERVER_LOGNAME
 
 if [[ $ORIGIN_SERVER_APACHE -eq 1 ]]; then
+  # Start server async, sleep, then kill
   sudo /etc/init.d/apache2 start
   sleep $ORIGIN_SERVER_DURATION
   sudo /etc/init.d/apache2 stop
+  
+  # Copy server access log
+  sudo cp /var/log/apache2/access.log $TMP_DATA/$ORIGIN_SERVER_LOGNAME
+  sudo rm /var/log/apache2/access.log
 
 else
   (python3 $DETER_HOME/multislowserver.py -p $ORIGIN_SERVER_PORT -d 1 -x True > $TMP_DATA/$ORIGIN_SERVER_LOGNAME 2>&1) &
