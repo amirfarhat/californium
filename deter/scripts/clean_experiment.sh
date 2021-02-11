c30fa64644890baf0ee633bf89c05e334c536a3b
Amir Fahat <amirf@mit.edu>
2021-02-11 15:50:33 -0500

    Update experiemnt cleaner

--- deter/scripts/clean_experiment.sh
+++ deter/scripts/clean_experiment.sh
@@ -6,37 +6,24 @@ if [[ -z "$CF_HOME" ]]; then
   exit 1
 fi
 
+DATA_DIR=$CF_HOME/deter/expdata/real/proxy
 SCRIPTS_DIR=$CF_HOME/deter/scripts
-cd $SCRIPTS_DIR
 
-exp_dir=$1
+experiment_name=$1
+
+# Construct full path to experiment directory
+exp_dir="$DATA_DIR/$experiment_name"
+
+cd $SCRIPTS_DIR
 
 for D in $exp_dir/*/; do
   if [[ -d $D ]]; then
     echo "Looking for processed files in $D..."
 
-    # Dumps
-    if [[ -f $D/processed_attacker_dump.out ]]; then
-      echo "Removing processed_attacker_dump.out..."
-      rm $D/processed_attacker_dump.out
-    fi
-    if [[ -f $D/processed_proxy_dump.out ]]; then
-      echo "Removing processed_proxy_dump.out..."
-      rm $D/processed_proxy_dump.out
-    fi
+    # Processed dumps
+    rm $D/*_dump.out
     
     # Csvs
-    if [[ -f $D/attacker_messages.csv ]]; then
-      echo "Removing attacker_messages.csv..."
-      rm $D/attacker_messages.csv
-    fi
-    if [[ -f $D/proxy_messages.csv ]]; then
-      echo "Removing proxy_messages.csv..."
-      rm $D/proxy_messages.csv
-    fi
-    if [[ -f $D/proxy_log_messages.csv ]]; then
-      echo "Removing proxy_log_messages.csv..."
-      rm $D/proxy_log_messages.csv
-    fi
+    rm $D/*_messages.csv
   fi
 done
