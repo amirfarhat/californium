80bd44ab5313b7db0572588e3dec7799a1e817e8
Amir Fahat <amirf@mit.edu>
2021-02-11 14:53:04 -0500

    Fix weird install bug

--- deter/scripts/node_setup.sh
+++ deter/scripts/node_setup.sh
@@ -2,6 +2,9 @@
 
 source /proj/MIT-DoS/exp/coap-setup/deps/californium/deter/scripts/config.sh
 
+# Fix weird bug where we can't apt install
+sudo dpkg --configure -a
+
 # Expect:
 # Forked californium from source: https://github.com/amirfarhat/californium
 if [[ ! -d $CF_HOME ]]; then
