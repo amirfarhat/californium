dd3ea196f14915da767ac84c2e0f097bff2d1695
Amir Fahat <amirf@mit.edu>
2021-02-12 15:16:06 -0500

    Maybe bug in run tcpdump

--- deter/scripts/sbin/run_tcpdump.sh
+++ deter/scripts/sbin/run_tcpdump.sh
@@ -4,35 +4,35 @@ source /proj/MIT-DoS/exp/coap-setup/deps/californium/deter/scripts/config.sh
 
 node_type=$1
 
-if [ $node_type = "proxy" ]; then
+if [[ $node_type == "proxy" ]]; then
   sleep_amt=$PROXY_DURATION
   rm -f $TMP_DATA/$PROXY_TCPDUMP
   touch $TMP_DATA/$PROXY_TCPDUMP
   eval "tcpdump -n -i any port '($ORIGIN_SERVER_PORT or $PROXY_COAP_PORT)' -w $TMP_DATA/$PROXY_TCPDUMP &"
 
-elif [ $node_type = "attacker" ]; then
+elif [[ $node_type == "attacker" ]]; then
   sleep_amt=$PROXY_DURATION
   rm -f $TMP_DATA/$ATTACKER_TCPDUMP
   touch $TMP_DATA/$ATTACKER_TCPDUMP
   tcpdump -n -i any udp port $PROXY_COAP_PORT -w $TMP_DATA/$ATTACKER_TCPDUMP &
 
-elif [ $node_type = "origin_server" ]; then
+elif [[ $node_type = "origin_server" ]]; then
   sleep_amt=$ORIGIN_SERVER_DURATION
   rm -f $TMP_DATA/$ORIGIN_SERVER_TCPDUMP
   touch $TMP_DATA/$ORIGIN_SERVER_TCPDUMP
   tcpdump -n -i any port $ORIGIN_SERVER_PORT -w $TMP_DATA/$ORIGIN_SERVER_TCPDUMP &
 
-elif [ $node_type = "receiver" ]; then
+elif [[ $node_type == "receiver" ]]; then
   sleep_amt=$RECEIVER_DURATION
   rm -f $TMP_DATA/$RECEIVER_TCPDUMP
   touch $TMP_DATA/$RECEIVER_TCPDUMP
   tcpdump -n -i any udp port $RECEIVER_COAP_PORT -w $TMP_DATA/$RECEIVER_TCPDUMP &
 
-elif [ $node_type = "client" ]; then
+elif [[ $node_type == "client" ]]; then
   sleep_amt=$CLIENT_DURATION
   rm -f $TMP_DATA/$CLIENT_TCPDUMP
   touch $TMP_DATA/$CLIENT_TCPDUMP
-  tcpdump -n -i any udp -w $TMP_DATA/$CLIENT_TCPDUMP &
+  tcpdump -n -i any udp port $PROXY_COAP_PORT -w $TMP_DATA/$CLIENT_TCPDUMP &
 
 else
   echo "Unknown parameter"
