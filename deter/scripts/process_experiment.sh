8afa94b83cd2c9ccaba3aa524e773a188cfaf77f
Amir Fahat <amirf@mit.edu>
2021-02-13 21:38:26 -0500

    Fix bug in processor

--- deter/scripts/process_experiment.sh
+++ deter/scripts/process_experiment.sh
@@ -75,48 +75,48 @@ pids=()
 for D in $exp_dir/*/; do
   if [[ -d $D ]]; then
     echo "Processing data into csvs in $D..."
-    # Assert presence of processed tcpdumps and proxy log
-    if [[ ! -f $D/processed_attacker_dump.out ]]; then
-      echo "Could not find attacker dump in $D"
-      exit 1
-    fi
-    if [[ ! -f $D/processed_proxy_dump.out ]]; then
-      echo "Could not find proxy dump in $D"
-      exit 1
-    fi
-    if [[ ! -f $D/processed_client_dump.out ]]; then
-      echo "Could not find client dump in $D"
-      exit 1
-    fi
-    if [[ ! -f $D/proxy.log ]]; then
-      echo "Could not find proxy log in $D"
-      exit 1
-    fi
+    # # Assert presence of processed tcpdumps and proxy log
+    # if [[ ! -f $D/processed_attacker_dump.out ]]; then
+    #   echo "Could not find attacker dump in $D"
+    #   exit 1
+    # fi
+    # if [[ ! -f $D/processed_proxy_dump.out ]]; then
+    #   echo "Could not find proxy dump in $D"
+    #   exit 1
+    # fi
+    # if [[ ! -f $D/processed_client_dump.out ]]; then
+    #   echo "Could not find client dump in $D"
+    #   exit 1
+    # fi
+    # if [[ ! -f $D/proxy.log ]]; then
+    #   echo "Could not find proxy log in $D"
+    #   exit 1
+    # fi
     # Now issue tasks to process these
     if [[ ! -f $D/proxy_log_messages.csv ]]; then
       (python3 data_processor.py -t proxy_log -i $D/proxy.log -o $D/proxy_log_messages.csv) &
       pids+=($!)
     fi
     if [[ ! -f $D/proxy_messages.csv ]]; then
-      if [[ -f $D/proxy_dump.pcap ]]; then
+      if [[ -f $D/processed_proxy_dump.pcap ]]; then
         (python3 data_processor.py -t proxy_dump -i $D/processed_proxy_dump.out -o $D/proxy_messages.csv) &
         pids+=($!)
       fi
     fi
     if [[ ! -f $D/attacker_messages.csv ]]; then
-      if [[ -f $D/attacker_dump.pcap ]]; then
+      if [[ -f $D/processed_attacker_dump.pcap ]]; then
         (python3 data_processor.py -t attacker_dump -i $D/processed_attacker_dump.out -o $D/attacker_messages.csv) &
         pids+=($!)
       fi
     fi
     if [[ ! -f $D/receiver_messages.csv ]]; then
-      if [[ -f $D/receiver_dump.pcap ]]; then
+      if [[ -f $D/processed_receiver_dump.pcap ]]; then
         (python3 data_processor.py -t receiver_dump -i $D/processed_receiver_dump.out -o $D/receiver_messages.csv) &
         pids+=($!)
       fi
     fi
     if [[ ! -f $D/client_messages.csv ]]; then
-      if [[ -f $D/client_dump.pcap ]]; then
+      if [[ -f $D/processed_client_dump.pcap ]]; then
         (python3 data_processor.py -t client_dump -i $D/processed_client_dump.out -o $D/client_messages.csv) &
         pids+=($!)
       fi
